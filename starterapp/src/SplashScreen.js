0e4efa44bc76c2ac63e09fce80aae243258a4e95
Abdullah Bin Amir <75297261+AbdullahBinAmir@users.noreply.github.com>
2023-01-08 23:57:45 +0500

    changes

--- starterapp/src/SplashScreen.js
+++ starterapp/src/SplashScreen.js
@@ -1,14 +1,47 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+import React from 'react';
 
 const SplashScreen = () => {
   return (
-    <View>
-      <Text>SplashScreen</Text>
+    <View style={styles.container}>
+      <ImageBackground
+        source={require('../assets/bg-img.jpg')}
+        resizeMode="center"
+        style={styles.image}>
+        <TouchableOpacity style={styles.btn}>
+          <Text style={{fontSize:14,fontWeight:'bold',color:'black'}}>Get Started</Text>
+        </TouchableOpacity>
+      </ImageBackground>
     </View>
-  )
-}
+  );
+};
 
-export default SplashScreen
+export default SplashScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFC803',
+  },
+  image: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  btn: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 300,
+    borderWidth: 2,
+    alignSelf: 'center',
+    paddingVertical: 5,
+    paddingHorizontal:10,
+    borderRadius: 10,
+  },
+});
